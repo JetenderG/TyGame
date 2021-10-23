fc1ff052c4479290fdfce0c45ea5a4c4324bfc67
Jetender <g.jetender@gmail.com>
2021-10-23 00:07:45 -0400

    script.js works still need some addition code and styling is nagai

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,9 +1,13 @@
-const keys = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","space"];
-const letterContainer = document.querySelector('.letters-container')
-const pargraphContainer = document.querySelector(".paragraph-data")
+const keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "space"];
+const letterContainer = document.querySelector('.letters-container');
+const pargraphContainer = document.querySelector(".paragraph-data");
 const lettersPressed = document.querySelectorAll('key');
-const generateText = document.querySelector('.generateText')
+const generateText = document.querySelector('.generateText');
 const typingContainer = document.querySelector(".typing-container");
+const btnGenerate = document.querySelector('.generateText');
+const userTyped = document.getElementsByClassName("user-text")[0];
+
+//This is where we populate the page with letters and symbols respresenting the keys from the array from which represents the keyboard
 keys.forEach(element => {
 
     let letter = document.createElement('button');
@@ -16,45 +20,67 @@ keys.forEach(element => {
 
 
 
-let word_find_test = "Good Morning";
-let word_empty_arg = [];
-let createEmptyWordArg = (word) =>{
-    for (let i = 0; i < word.length; i++){
-    }   
-}
-createEmptyWordArg(word_find_test)
-
-let checkTyping = (text, word) =>{
-    
-    let countWord = word.length;
-
-    
-}
-
-
-generateText.addEventListener('click', ()=>{
-    console.log("sfse")
-  fetch("/api/facts")
-  .then(response => response.json())
-  .then(data => {
-      pargraphContainer.append(data.sentence)
-      document.addEventListener('keydown', (event)=>{        
-
-        if (keys.includes(event.key) || event.key === " "){
-            console.log(event.key)
-            let pressedKey = document.querySelector(`#${event.key === " " ? "space" : event.key }`);
-            pressedKey.classList.add("button_active")
-    
-            typingContainer.append(event.key)
+let compareCharacterAt = (txt_char, word_char) =>{
+    console.log(txt_char, word_char)
+    switch (txt_char === word_char){
+        case true:
+             break;
+        case false:
+            console.log("he")
+            let regex = new RegExp(`[${txt_char}]` )
+            console.log(regex)
+            userTyped.textContent.replace(regex, `<span style = "color:red">${txt_char}</span>`)
+            break;   
         }
-    })
-    document.addEventListener('keyup', (event)=>{
-        if (keys.includes(event.key) || event.key === " "){
-            let pressedKey = document.querySelector(`#${event.key === " " ? "space" : event.key }`);
-            pressedKey.classList.remove("button_active")
-        }
-    })
+}
+
+let checkTyping = (text, word) => {
+    console.log(String(text), word)
+    if(text === word){
+        alert("good")
+    }
+    for (let i = 0; i < text.length; i++){
+        console.log("Looped")
+        compareCharacterAt(text[i],word[i])
+    }
 
 
-  });
+}
+
+
+generateText.addEventListener('click', () => {
+    btnGenerate.disabled = true;
+    fetch("/api/facts")
+        .then(response => response.json())
+        .then(data => {
+            pargraphContainer.append(data.sentence)
+            document.addEventListener('keydown', (event) => {
+                console.log(event.key)
+                if (keys.includes(event.key) || event.key === " ") {
+                    console.log(userTyped)
+                    let pressedKey = document.querySelector(`#${event.key === " " ? "space" : event.key }`);
+                    pressedKey.classList.add("button_active")
+
+                    userTyped.append(event.key)
+                } else if (event.key === "Backspace") {
+                    let userWritten = userTyped.textContent;
+                   userTyped.textContent = userWritten.substring(0, userWritten.length - 1)
+
+                }
+
+                            checkTyping(userTyped.textContent, data.sentence)
+
+            })
+
+
+
+            document.addEventListener('keyup', (event) => {
+                if (keys.includes(event.key) || event.key === " ") {
+                    let pressedKey = document.querySelector(`#${event.key === " " ? "space" : event.key }`);
+                    pressedKey.classList.remove("button_active")
+                }
+            })
+
+
+        });
 })
--- routes/api/factRoutes.js
+++ routes/api/factRoutes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get("/facts", (req, res,next) => {
 
-    res.json(data = {sentence : "Hello this is working out fine"})
+    res.json(data = {sentence : "hello this is working out fine"})
 });
 
 
