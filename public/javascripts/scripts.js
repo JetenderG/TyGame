54fc598f9037b350e6d4a05aa54bd21ab2272b9a
Jetender <g.jetender@gmail.com>
2021-11-04 01:11:07 -0400

    Added a better function format tho need to reorganize code plus addition site improvement

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -8,44 +8,116 @@ const btnGenerate = document.querySelector('.generateText');
 const userTyped = document.getElementsByClassName("user-text")[0];
 
 //This is where we populate the page with letters and symbols respresenting the keys from the array from which represents the keyboard
-keys.forEach(element => {
 
+let checkTyping = (text, word) => {
+    console.log("Checking Typing Alert")
+    console.log(String(text), word)
+    let userText = text.textContent;
+    let wordText = word.sentence;
+    for (let i = 0; i < userText.length; i++) {
+        console.log("h")
+        switch (userText[i] === wordText[i]) {
+            case true:
+                break;
+            case false:
+                let span = document.createElement("span");
+                span.className = "incorrectGS";
+                span.textContent = userText[i]
+                replaceChar(text, i, span)
+                console.log(span)
+                break;
+        }
+    }
+}
+
+
+let attemptOne = (typed, word) => {
+    console.log("AttemptOne function invokeds")
+    if (typed.length === 0) {
+        console.log("None Typed Yet")
+    } else {
+        looped(typed, word)
+        .then(data => {
+         return replaceCharacter(data, typed)  
+        })
+        .then(data => {
+            typingContainer.innerHTML = data;
+        })
+    }
+
+}
+
+let looped = (typed, word) => {
+    let errorResults = [];
+    console.log(typed, word)
+    console.log("Looped Invoked")
+    if (typed.length === 0 ){
+        return Promise.reject("Error")
+    }
+    for (let i = 0; i < typed.length; i++) {
+        switch (typed[i] === word[i]) {
+            case true:
+                console.log(typed[i], word[i])
+                break;
+            case false:
+                let errSpell = {
+                    letter: typed[i],
+                    index: i
+                }
+                errorResults.push(errSpell)
+                console.log(errorResults)
+                break;
+
+        }
+
+    }
+    return Promise.resolve(errorResults);
+}
+
+
+let replaceCharacter = (arg, typed) =>{
+    
+    let reConstructive = typed.split("");
+    arg.forEach(char => {
+        let newCharacter =   document.createElement("span");
+            newCharacter.textContent = char.letter;
+            console.log(char.index)
+            Array
+            reConstructive[char.index] = newCharacter
+
+                   
+
+        });
+         let newString =   reConstructive.join("")
+        console.log(reConstructive)
+        return Promise.resolve(newString);
+}
+
+let replaceChar = (word, index, element) => {
+    console.log("replaceChar working , element name" + element)
+    console.log(element)
+
+    let argWord = Array.from(word.textContent);
+    argWord[index] = element;
+    // word.innerHTML = " ";
+    word.append(element);
+    console.log(word)
+    return word;
+
+
+
+}
+
+
+
+
+keys.forEach(element => {
     let letter = document.createElement('button');
     letter.setAttribute("class", "key")
     letter.setAttribute("id", `${element}`)
     letter.textContent = element;
-
     letterContainer.appendChild(letter)
 });
-
-
-
-let compareCharacterAt = (txt_char, word_char) =>{
-    console.log(txt_char, word_char)
-    switch (txt_char === word_char){
-        case true:
-             break;
-        case false:
-            console.log("he")
-            let regex = new RegExp(`[${txt_char}]` )
-            console.log(regex)
-            userTyped.textContent.replace(regex, `<span style = "color:red">${txt_char}</span>`)
-            break;   
-        }
-}
-
-let checkTyping = (text, word) => {
-    console.log(String(text), word)
-    if(text === word){
-        alert("good")
-    }
-    for (let i = 0; i < text.length; i++){
-        console.log("Looped")
-        compareCharacterAt(text[i],word[i])
-    }
-
-
-}
 
 
 generateText.addEventListener('click', () => {
@@ -57,30 +129,21 @@ generateText.addEventListener('click', () => {
             document.addEventListener('keydown', (event) => {
                 console.log(event.key)
                 if (keys.includes(event.key) || event.key === " ") {
-                    console.log(userTyped)
                     let pressedKey = document.querySelector(`#${event.key === " " ? "space" : event.key }`);
                     pressedKey.classList.add("button_active")
-
                     userTyped.append(event.key)
                 } else if (event.key === "Backspace") {
                     let userWritten = userTyped.textContent;
-                   userTyped.textContent = userWritten.substring(0, userWritten.length - 1)
-
+                    userTyped.textContent = userWritten.substring(0, userWritten.length - 1)
                 }
-
-                            checkTyping(userTyped.textContent, data.sentence)
-
+                //  checkTyping(userTyped, data)
+                attemptOne(userTyped.textContent, data.sentence)
             })
-
-
-
             document.addEventListener('keyup', (event) => {
                 if (keys.includes(event.key) || event.key === " ") {
                     let pressedKey = document.querySelector(`#${event.key === " " ? "space" : event.key }`);
                     pressedKey.classList.remove("button_active")
                 }
             })
-
-
         });
 })
